cb071826c7723b74b127de19ef98b96f9f90c0d3
Caryappa B C <caryappa.cari@gmail.com>
2024-12-16 09:54:11 +0530

    added mongodb package and basic get and post tested

---
+++ app/api/dbtest/route.js
@@ -0,0 +1,46 @@
+import { MongoClient } from 'mongodb';
+
+
+export async function GET(request) {
+    // Do whatever you want
+    const client = new MongoClient(process.env.MONGODB_URI);
+    try{
+    await client.connect();
+    console.log("Connected successfully to server");
+
+    const db = client.db('DBTest');
+    const collection = db.collection('tests');
+    const users = await collection.find({}).toArray();
+    return new Response(users, {
+        status: 200
+      })
+    }
+    catch (err) {
+    console.error(err.stack);
+    } 
+    
+  }
+
+  export async function POST(request) {
+    // Do whatever you want
+    const client = new MongoClient(process.env.MONGODB_URI);
+    try{
+    await client.connect();
+    console.log("Connected successfully to server");
+    const formData = await request.formData()
+    const name = formData.get('name')
+    const age = formData.get('age')
+    const db = client.db('DBTest');
+    const collection = db.collection('tests');
+    const insertResult = await collection.insertOne({ name, age });
+    return new Response({insertResult}, {
+        status: 200
+    })
+    }
+    catch (err) {
+    console.error(err.stack);
+    } 
+    
+  }
+
+
--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,11 @@
 import { Button } from "@/components/ui/button"
 
-export default function Home() {
+export default async function Home() {
+
+  const data = await fetch("http://localhost:3000/api/hello");
+  console.log(data);
+
+
   return (
     <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
       <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
